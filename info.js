c3027aeec8899c61b692e8a84bacdb3892377bd2
AndresDrimer <andresdrimer@hotmail.com>
2022-12-05 11:46:32 -0300

    firts commit

---
+++ info.js
@@ -0,0 +1,421 @@
+let info = {
+  name: "Andrés Drimer",
+  logo_name: "Andrés Drimer",
+  flat_picture: require("./src/assets/circulo-cara-andres-no-fondo.png"),
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false,
+    },
+  },
+  description:
+    "Hello, I'm Andrés Drimer, an enthusiastic front-end developer based in Buenos Aires, Argentina. UI/UX Designer 3+ experience with Figma, Photoshop, Illustrator, WordPress. Frontend Developer 2+ experience with CSS3, HTML5 and Javascript, 1+ in Vuejs. Video and Music Editor 6+ with Premiere, Nuendo, Cubase, Protools. I´ve started my carrer as a musician and I´ve been within music industry as a a manager and executive producer for 10+, including team management for local and international gigs.",
+  links: {
+    linkedin: "https://www.linkedin.com/in/andres-drimer/",
+    github: "https://github.com/AndresDrimer",
+    behance: "",
+    resume:
+      "https://drive.google.com/file/d/1VUtBNjYQ467VAB0ztKw6IHewDk6OeVyJ/view?usp=sharing",
+  },
+  education: [
+    {
+      name: "Codo a Codo 4.0",
+      place: "",
+      date: "",
+      degree: "Full Stack PYTHON",
+      gpa: "",
+      description: "",
+      skills: [
+        "HTML5",
+        "CSS3",
+        "Bootstrap 5",
+        "Javascript ES6",
+        "Vue",
+        "Python",
+        "Git",
+        "Scrum",
+        "MySQL",
+      ],
+    },
+    {
+      name: "Codo a Codo 4.0",
+      place: "",
+      date: "(on going)",
+      degree: "Django",
+      gpa: "",
+      description: "",
+      skills: ["Pyhton", "Django"],
+    },
+    {
+      name: "Academia BA",
+      place: "",
+      date: "",
+      degree: "Digital marketing & Financial education",
+      gpa: "",
+      description: "",
+      skills: ["", ""],
+    },
+  ],
+  experience: [
+    {
+      name: "Freelancer",
+      place: "Fever-Upwork",
+      date: "2020 - present",
+      position: "Web developer",
+      description:
+        "I´ve worked in many projects creating and design fully responsive sites to fullfil clients´ requirements and expectations",
+      skills: [
+        "HTML5",
+        "CSS3",
+        "Bootstrap5",
+        "JavaScript ES6",
+        "TypeScript",
+        "VueJS",
+        "Figma",
+        "Illustrator",
+        "Photoshop",
+        "Git",
+        "Scrum",
+      ],
+    },
+    {
+      name: "Café de las Ciudades",
+      place: "",
+      date: "2020 - 2022",
+      position: "Web developer and commercial management",
+      description:
+        "I´ve designed and created implementation for online book store and digital magazine. Also had commercial responsabilities, including marketing and ad campaigns and coorditantion between . Accounting tasks also included.",
+      skills: [
+        "HTML5",
+        "CSS3",
+        "Bootstrap5",
+        "JavaScript ES6",
+        "Figma",
+        "Photoshop",
+        "Wordpress",
+        "Excel",
+      ],
+    },
+    {
+      name: "Max Aguirre",
+      place: "",
+      date: "2021 - 2022",
+      position: "Web developer and commercial management",
+      description:
+        "Web Development, design and implementation for artist personal web and for his duet spectacle´s site. Management includes commercial coordination with clients, organization for shows including international gigs and subsidy management, coordination for artist´s courses  ",
+      skills: [
+        "HTML5",
+        "CSS3",
+        "Bootstrap5",
+        "JavaScript ES6",
+        "Figma",
+        "Photoshop",
+      ],
+    },
+  ],
+  skills: [
+    {
+      title: "Front-end",
+      info: ["HTML5", "CSS3", "Bootstrap5", "Javascript Es6"],
+      icon: "fa fa-cubes",
+    },
+    {
+      title: "Web technologies",
+      info: ["Vue3 + Vite + Vue-Router"],
+      icon: "fas fa-laptop-code",
+    },
+    {
+      title: "UX/UI Design",
+      info: ["Figma", "Illustrator", "Photoshop"],
+      icon: "fa fa-pencil-square-o",
+    },
+    {
+      title: "Languages",
+      info: ["Python", "Javascript", "TypeScript"],
+      icon: "fa fa-code",
+    },
+  ],
+  portfolio_design: [
+    {
+      name: "Max Aguirre",
+      title: "Front-End",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/max/max-img-1.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-2.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-3.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-4.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-5.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-6.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-7.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/max/max-img-8.png"),
+          title: "",
+        },
+      ],
+      technologies: ["HTML5", "CSS3", "Bootstrap5","Photoshop"],
+      category: "design & code",
+      github: "",
+      date: "Jan, 2022",
+      visit: "https://maxaguirre.com.ar/",
+      description: "",
+    },
+    {
+      name: "Café de las Ciudades",
+      title: "Café de las Ciudades",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/cdlc/cdlc-img-1.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/cdlc/cdlc-img-2.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/cdlc/cdlc-img-3.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/cdlc/cdlc-img-4.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/cdlc/cdlc-img-5.png"),
+          title: "",
+        },
+        {
+          img: require("./src/assets/portfolio/cdlc/cdlc-img-6.png"),
+          title: "",
+        },
+      ],
+      technologies: ["Figma", "Illustrator", "WordPress", "TiendaNube"],
+      category: "UI/UX Design - CMS dev",
+      github: "",
+      date: "2021-2022",
+      visit: "https://cafedelasciudades.com.ar/",
+      description: "Design and implementation of Book Store for urbanism specialized book Publisher ´Cafe de las Ciudades´ - update and migration for their on-line Magazine in their 20th Anniversary. Implementation of their own e-commerce has returned in a sales growth up to 600%. The idea was to make a clear line between both plataforms to inspire round navigation of their users",
+    },
+    {
+      name: "Credit card design",
+      title: "Illustration",
+      pictures: [
+        {
+          img: require("./src/assets/designs/credit/behance cover.png"),
+          title: "MoodBoard",
+        },
+        {
+          img: require("./src/assets/designs/credit/degisning.png"),
+          title: "Degisning",
+        },
+        {
+          img: require("./src/assets/designs/credit/Dual tune.png"),
+          title: "Dual tune",
+        },
+        {
+          img: require("./src/assets/designs/credit/Glassmorphism.png"),
+          title: "Glassmorphism",
+        },
+        {
+          img: require("./src/assets/designs/credit/Gradiant.png"),
+          title: "Gradiant",
+        },
+        {
+          img: require("./src/assets/designs/credit/Monochrome.png"),
+          title: "Monochrome",
+        },
+        {
+          img: require("./src/assets/designs/credit/Neumorphism.png"),
+          title: "Neumorphism",
+        },
+      ],
+      technologies: ["Figma", "Illustrator"],
+      category: "Visual Design",
+      github: "",
+      date: "Dec, 2021",
+      visit: "",
+      description: "",
+    },
+    {
+      name: "BOOKSI",
+      title: "E-Book library - UI/UX Design",
+      pictures: [
+        {
+          img: require("./src/assets/designs/booksi/behance cover.png"),
+          title: "Cover",
+        },
+        {
+          img: require("./src/assets/designs/booksi/behance board.png"),
+          title: "MoodBoard",
+        },
+        {
+          img: require("./src/assets/designs/booksi/login.png"),
+          title: "login",
+        },
+        {
+          img: require("./src/assets/designs/booksi/register.png"),
+          title: "register",
+        },
+        {
+          img: require("./src/assets/designs/booksi/home page.png"),
+          title: "homepage",
+        },
+        {
+          img: require("./src/assets/designs/booksi/For you.png"),
+          title: "just for you",
+        },
+      ],
+      technologies: ["Figma", "Photoshop"],
+      category: "Visual Design",
+      github: "",
+      date: "Nov, 2021",
+      visit: "",
+      description: " BOOKSI is Ebook android app for easy read books.",
+    },
+    {
+      name: "Quiet Weather",
+      title: "UI/UX Design",
+      pictures: [
+        {
+          img: require("./src/assets/designs/weather/Wooden Hand iPhone 12 Pro.png"),
+          title: "MoodBoard",
+        },
+        {
+          img: require("./src/assets/designs/weather/artboard.png"),
+          title: "show",
+        },
+        {
+          img: require("./src/assets/designs/weather/weather app react native.png"),
+          title: "components",
+        },
+      ],
+      technologies: ["Figma", "Illustration"],
+      category: "Visual Design",
+      github: "",
+      date: "Nov, 2021",
+      visit: "",
+      description:
+        "Quiet Weather is a react native app by openweathermap api, i designed the ui/ux and coding it in react native, enjoy it!",
+    },
+    {
+      name: "Pill Reminder",
+      title: "Pill Reminder - Mockup Design",
+      pictures: [
+        {
+          img: require("./src/assets/designs/pill reminder/Web 1.png"),
+          title: "Moodboard",
+        },
+        {
+          img: require("./src/assets/designs/pill reminder/1.png"),
+          title: "Mockups 1",
+        },
+        {
+          img: require("./src/assets/designs/pill reminder/2.png"),
+          title: "Mockups 2",
+        },
+      ],
+      technologies: ["XD", "Illustrator"],
+      category: "Visual Design",
+      github: "",
+      date: "Apr, 2021",
+      visit: "",
+      description: "pill reminder app for nessecary drugs for the day.",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Diary App",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/diary/1.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/diary/2.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/diary/3.png"),
+        },
+      ],
+      technologies: ["react", "scss", "local storage"],
+      category: "Web App",
+      date: "Aug, 2021 - 20 days",
+      github: "https://github.com/mahy209/notes-react-app.git",
+      visit: "https://github.com/mahy209/notes-react-app.git",
+      description:
+        " Diary react app with some new features: Dark mode, upload images, tags, filter search with title/content/tag ",
+    },
+    {
+      name: "Online Examination",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/online examination/exam (1).png"),
+        },
+        {
+          img: require("./src/assets/portfolio/online examination/exam (2).png"),
+        },
+        {
+          img: require("./src/assets/portfolio/online examination/exam (3).png"),
+        },
+        {
+          img: require("./src/assets/portfolio/online examination/exam (4).png"),
+        },
+        {
+          img: require("./src/assets/portfolio/online examination/exam (5).png"),
+        },
+      ],
+      technologies: ["HTML", "CSS", "JavaSript", "JQuery", "PHP", "MySQL"],
+      category: "Web App",
+      date: "Apr, 2021 - May, 2021",
+      github: "https://github.com/mahy209/online-examination.git",
+      visit: "https://github.com/mahy209/online-examination.git",
+      description:
+        "Online Examination System Today Online Examination System has become a fast growing examination method because of its speed and accuracy. It is also needed less manpower to execute the examination. Almost all organizations now-a-days, are conducting their objective exams by online examination system, it saves students time in examinations. Organizations can also easily check the performance of the student that they give in an examination. As a result of this, organizations are releasing results in less time. It also helps the environment by saving paper. According to today’s requirement, online examination project in php is very useful to learn it. What is an online examination system? In an online examination system examine get their user id and password with his/her admit card. This id is already saved in the examination server. When examine login to the server he/she get his/her profile already register. On the certain time examine gets the message to start the examination. All answers given by examine are saved into the server with his/her profile information. Online examination system also allows to correct the answer if the examine needed to change any answer in the examination time duration, however, after the time duration any change will not allow. This also makes c checking the answer easy and error proof as computers are more accurate than man and provide fast results too. Php is a web base language so we can create an online examination system in PHP. Administrator of Online Examination has multiple features such as Add, Delete, Update Topics and Question. To Login as Admin put inside your browser.",
+    },
+    {
+      name: "MyFlix.com",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/myflix.com/1.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/myflix.com/2.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/myflix.com/3.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/myflix.com/4.png"),
+        },
+      ],
+      technologies: ["react", "css", "TMDB"],
+      category: "Web App",
+      date: "Aug, 2021 - 10 days",
+      github: "https://github.com/mahy209/MyFlix-react-app.git",
+      visit: "https://github.com/mahy209/MyFlix-react-app.git",
+      description:
+        " flix.com is a netflix clone using React JS and TMDB with courusal and search engine. ",
+    },
+  ],
+};
+
+export default info;
